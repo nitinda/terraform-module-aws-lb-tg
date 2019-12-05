8e29627c894df5a5a811321a6ad01a2380586961
nitin-das <nitin.das@thomson.co.uk>
2019-12-05 14:24:48 +0000

    Modified code

---
+++ variables.tf
@@ -0,0 +1,37 @@
+## Target Group
+variable "name" {
+  description = "The name of the target group."
+}
+
+variable "target_type" {
+  description = "The type of target that you must specify when registering targets with this target group"
+}
+
+variable "port" {
+  description = "The port on which targets receive traffic, unless overridden when registering a specific target."
+}
+
+variable "protocol" {
+  description = "The protocol to use for routing traffic to the targets."
+}
+
+variable "vpc_id" {
+  description = "The identifier of the VPC in which to create the target group."
+}
+
+variable "deregistration_delay" {
+  description = "The amount time for Elastic Load Balancing to wait before changing the state of a deregistering target from draining to unused."
+}
+
+variable "health_check" {
+  description = "A Health Check block."
+  type        = map(string)
+}
+
+## Tags
+
+variable "common_tags" {
+  description = "Resources Tags"
+  type        = map(string)
+}
+
