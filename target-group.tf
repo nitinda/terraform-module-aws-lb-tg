754c4d746aee1930e1798efe488976c1a21e2e06
nitin-das <nitin.das@thomson.co.uk>
2020-04-17 14:18:41 +0100

    Modified Code

--- target-group.tf
+++ target-group.tf
@@ -1,10 +1,24 @@
 resource "aws_lb_target_group" "lb_target_group" {
-  name                 = var.name
-  port                 = var.port
-  protocol             = var.protocol
-  vpc_id               = var.vpc_id
-  target_type          = var.target_type
-  deregistration_delay = var.deregistration_delay
+  name                               = var.name
+  name_prefix                        = var.name_prefix
+  port                               = var.port
+  protocol                           = var.protocol
+  vpc_id                             = var.vpc_id
+  deregistration_delay               = var.deregistration_delay
+  load_balancing_algorithm_type      = var.load_balancing_algorithm_type
+  lambda_multi_value_headers_enabled = var.lambda_multi_value_headers_enabled
+  proxy_protocol_v2                  = var.proxy_protocol_v2
+
+
+  dynamic "stickiness" {
+    for_each = var.stickiness
+    content {
+      enabled           = lookup(stickiness.value, "type", null)
+      healthy_threshold = lookup(stickiness.value, "cookie_duration", null)
+      interval          = lookup(stickiness.value, "enabled", null)
+    }
+  }
+
   dynamic "health_check" {
     for_each = var.health_check
     content {
@@ -19,5 +33,11 @@ resource "aws_lb_target_group" "lb_target_group" {
       unhealthy_threshold = lookup(health_check.value, "unhealthy_threshold", null)
     }
   }
-  tags = var.tags
+
+  target_type = var.target_type
+  tags        = var.tags
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
