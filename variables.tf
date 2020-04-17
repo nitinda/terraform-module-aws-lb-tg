ef2a1630886f6c21d09a7f27d24425c4935e323f
nitin-das <nitin.das@thomson.co.uk>
2020-04-17 14:24:15 +0100

    Modified Code

--- variables.tf
+++ variables.tf
@@ -9,22 +9,20 @@ variable "name_prefix" {
   default     = null
 }
 
-
-variable "target_type" {
-  description = "The type of target that you must specify when registering targets with this target group"
-}
-
 variable "port" {
   description = "The port on which targets receive traffic, unless overridden when registering a specific target."
   type        = number
+  default     = null
 }
 
 variable "protocol" {
   description = "The protocol to use for routing traffic to the targets."
+  default     = null
 }
 
 variable "vpc_id" {
   description = "The identifier of the VPC in which to create the target group."
+  default     = null
 }
 
 variable "deregistration_delay" {
@@ -33,15 +31,40 @@ variable "deregistration_delay" {
   type        = number
 }
 
+variable "load_balancing_algorithm_type" {
+  description = "Determines how the load balancer selects targets when routing requests"
+  default     = null
+}
+
+variable "lambda_multi_value_headers_enabled" {
+  description = "Boolean whether the request and response headers exchanged between the load balancer and the Lambda function include arrays of values or strings"
+  default     = null
+}
+
+variable "proxy_protocol_v2" {
+  description = "Boolean to enable / disable support for proxy protocol v2 on Network Load Balancers"
+  default     = null
+}
+
+variable "stickiness" {
+  description = "A Stickiness block. Stickiness blocks are documented below. stickiness is only valid if used with Load Balancers of type Application"
+  default     = null
+}
+
 variable "health_check" {
   description = "A Health Check block."
   type        = any
+  default     = {}
 }
 
-## Tags
+variable "target_type" {
+  description = "The type of target that you must specify when registering targets with this target group"
+  default     = null
+}
 
 variable "tags" {
   description = "Resources Tags"
   type        = map(string)
+  default     = {}
 }
 
